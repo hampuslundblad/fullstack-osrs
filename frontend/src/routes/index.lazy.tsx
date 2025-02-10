755bf576d94f453e664394a3d09b27a5d1d604ab
hampuslundblad <31845093+hampuslundblad@users.noreply.github.com>
2025-02-10 14:21:22 +0100

    test docker image

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import { Button } from "@/components/ui/button";
+import Title from "@/components/ui/title";
 import { Separator } from "@radix-ui/react-separator";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
@@ -16,6 +17,7 @@ function RouteComponent() {
     <Layout title="Home">
       <div className="flex flex-col gap-4 mt-8">
         <div className="flex gap-4">
+          <Title> Test </Title>
           <Button
             className="self-start"
             variant={"default"}
